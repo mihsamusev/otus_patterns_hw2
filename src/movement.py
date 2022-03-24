b88b864cf3ffcbc68b41c564948333fd6d81974b
mihsamusev <mih.samusev@gmail.com>
2022-03-24 21:43:58 +0100

    turnable added

--- src/movement.py
+++ src/movement.py
@@ -1,50 +1,37 @@
-from abc import ABC, abstractmethod
-from typing import Protocol
+import abc
 
 from src.core import Vector
 
 
-class Movable(ABC):
+class ICommand(abc.ABC):
     """
-    Interface for all object capable of linear movement
+    Interface for executable command
     """
 
-    @property
-    @abstractmethod
-    def position(self) -> Vector:
-        ...
-
-    @position.setter
-    @abstractmethod
-    def position(self, value: Vector):
-        ...
-
-    @property
-    @abstractmethod
-    def velocity(self) -> Vector:
+    def execute(self):
         ...
 
 
-class Movable2(ABC):
+class Movable(abc.ABC):
 
     """
     Interface for all object capable of linear movement
     """
 
-    @abstractmethod
+    @abc.abstractmethod
     def get_position(self) -> Vector:
         ...
 
-    @abstractmethod
+    @abc.abstractmethod
     def set_position(self, value: Vector):
         ...
 
-    @abstractmethod
+    @abc.abstractmethod
     def get_velocity(self) -> Vector:
         ...
 
 
-class MoveCommand:
+class MoveCommand(ICommand):
     """
     Executes linear movement of a Movable object
     """
@@ -53,20 +40,46 @@ class MoveCommand:
         self._movable = movable
 
     def execute(self):
-        self._movable.position = self._movable.position + self._movable.velocity
-
-
-class MoveCommand2:
-    """
-    Executes linear movement of a Movable object
-    """
-
-    def __init__(self, movable: Movable2):
-        self._movable = movable
-
-    def execute(self):
         self._movable.set_position(
             self._movable.get_position() + self._movable.get_velocity()
+        )
+
+
+class Turnable(abc.ABC):
+    """
+    Interface for all objects capable of turning
+    All directions are ints with max direction resolution
+    """
+
+    @abc.abstractclassmethod
+    def get_direction(self) -> int:
+        ...
+
+    @abc.abstractclassmethod
+    def get_direction_max(self) -> int:
+        ...
+
+    @abc.abstractclassmethod
+    def set_direction(self, value: int):
+        ...
+
+    @abc.abstractclassmethod
+    def get_angular_velocity(self) -> int:
+        ...
+
+
+class TurnCommand(ICommand):
+    """
+    Execulte turn of a Turnable object
+    """
+
+    def __init__(self, turnable: Turnable):
+        self._turnable = turnable
+
+    def execute(self):
+        self._turnable.set_direction(
+            (self._turnable.get_direction() + self._turnable.get_angular_velocity())
+            % self._turnable.get_direction_max()
         )
 
 
--- tests/test_movement.py
+++ tests/test_movement.py
@@ -1,13 +1,14 @@
+import unittest
 from unittest import TestCase
 from unittest.mock import Mock
 
 from src.core import Vector
-from src.movement import Movable, Movable2, MoveCommand, MoveCommand2
+from src.movement import Movable, MoveCommand, Turnable, TurnCommand
 
 
-class MockMovable(Movable2):
-    _position: Vector
-    _velocity: Vector
+class FakeMovable(Movable):
+    _position: Vector = Vector([0.0, 0.0])
+    _velocity: Vector = Vector([0.0, 0.0])
 
     def get_position(self) -> Vector:
         return self._position
@@ -19,41 +20,98 @@ class MockMovable(Movable2):
         return self._velocity
 
 
-class MoveTestCase(TestCase):
+class MoveCommandTestCase(TestCase):
     def test_move_sucessful(self):
-        body = Mock(spec_set=Movable)
-        body.position = Vector([12.0, 5.0])
-        body.velocity = Vector([-7, 3])
-
-        MoveCommand(body).execute()
-        self.assertTrue(body.position == Vector([5, 8]))
-
-    def test_move_sucessful2(self):
-        body = MockMovable()
+        body = FakeMovable()
         body._position = Vector([12.0, 5.0])
         body._velocity = Vector([-7, 3])
-        MoveCommand2(body).execute()
 
+        MoveCommand(body).execute()
         self.assertTrue(body.get_position() == Vector([5, 8]))
 
     def test_move_raises_when_get_position_is_not_implemented(self):
-        body = Mock(spec_set=Movable2)
-        body.get_position.side_effect = NotImplementedError()
-        body.get_velocity.return_value = Vector([-5.0, 3.0])
+        body = Mock(spec_set=Movable)
 
-        self.assertRaises(NotImplementedError, MoveCommand2(body).execute)
+        body.get_position.side_effect = NotImplementedError()
+        self.assertRaises(Exception, MoveCommand(body).execute)
 
     def test_move_raises_when_get_velocity_is_not_implemented(self):
-        body = Mock(spec_set=Movable2)
-        body.get_position.return_value = Vector([12.0, 5.0])
-        body.get_velocity.side_effect = NotImplementedError()
+        body = Mock(spec_set=Movable)
 
-        self.assertRaises(NotImplementedError, MoveCommand2(body).execute)
+        body.get_velocity.side_effect = NotImplementedError()
+        self.assertRaises(Exception, MoveCommand(body).execute)
 
     def test_move_raises_when_set_position_is_not_implemented(self):
-        body = Mock(spec_set=Movable2)
-        body.get_position.return_value = Vector([12.0, 5.0])
-        body.get_velocity.return_value = Vector([-5.0, 3.0])
-        body.set_position.side_effect = NotImplementedError()
+        body = Mock(spec_set=Movable)
 
-        self.assertRaises(NotImplementedError, MoveCommand2(body).execute)
+        body.set_position.side_effect = NotImplementedError()
+        self.assertRaises(Exception, MoveCommand(body).execute)
+
+
+class FakeTurnable(Turnable):
+    _direction: int = 0
+    _angular_velocity: int = 0
+    _max_directions: int = 100
+
+    def get_direction(self) -> int:
+        return self._direction
+
+    def set_direction(self, value: int):
+        self._direction = value
+
+    def get_angular_velocity(self) -> int:
+        return self._angular_velocity
+
+    def get_direction_max(self) -> int:
+        return self._max_directions
+
+
+class TurnCommandTestCase(unittest.TestCase):
+    def test_start_at_zero_turn_half_circle_get_to_half_circle(self):
+        body = FakeTurnable()
+        body._angular_velocity = 5
+        body._max_directions = 10
+
+        TurnCommand(body).execute()
+        self.assertEqual(body.get_direction(), 5)
+
+    def test_start_at_zero_turn_by_3_half_circles_get_to_half_circle(self):
+        body = FakeTurnable()
+        body._angular_velocity = 15
+        body._max_directions = 10
+
+        TurnCommand(body).execute()
+        self.assertEqual(body.get_direction(), 5)
+
+    def test_start_at_nonzero_turn_full_circle_get_back_to_start(self):
+        body = FakeTurnable()
+        body._direction = 2
+        body._angular_velocity = 10
+        body._max_directions = 10
+
+        TurnCommand(body).execute()
+        self.assertEqual(body.get_direction(), 2)
+
+    def test_turn_raises_when_get_direction_is_not_implemented(self):
+        body = Mock(spec_set=Turnable)
+        body.get_direction.side_effect = NotImplementedError()
+
+        self.assertRaises(Exception, TurnCommand(body).execute)
+
+    def test_turn_raises_when_set_direction_is_not_implemented(self):
+        body = Mock(spec_set=Turnable)
+        body.set_direction.side_effect = NotImplementedError()
+
+        self.assertRaises(Exception, MoveCommand(body).execute)
+
+    def test_turn_raises_when_get_velocity_is_not_implemented(self):
+        body = Mock(spec_set=Turnable)
+        body.get_angular_velocity.side_effect = NotImplementedError()
+
+        self.assertRaises(Exception, MoveCommand(body).execute)
+
+    def test_turn_raises_when_get_direction_max_is_not_implemented(self):
+        body = Mock(spec_set=Turnable)
+        body.get_direction_max.side_effect = NotImplementedError()
+
+        self.assertRaises(Exception, MoveCommand(body).execute)
